a3ef0478704cd88d32e449892c0f8a29ef9ed79d
Gabriel Matos <gabrielmatosprogramador@gmail.com>
2025-04-29 19:51:07 -0300

    Finalização da refatoração da classe client_socialnetwork

--- apps/client_socialnetwork/views.py
+++ apps/client_socialnetwork/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import ClientSocialnetwork
+from rest_framework import viewsets
+from .serializer import ClientSocialNetworkSerializer
 
-# Create your views here.
+class ClientSocialNetworkViewSet(viewsets.ModelViewSet):
+    queryset = ClientSocialnetwork.objects.all()
+    serializer_class = ClientSocialNetworkSerializer  
